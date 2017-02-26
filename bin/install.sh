405810f7d8564a9a5bd2c5a2df17c64dfc76b05e
Pedro Amador <pedroamador.rodriguez@gmail.com>
2017-02-26 20:58:20 +0100

    Add fastlane

--- bin/install.sh
+++ bin/install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 cd $(dirname $0)
 mkdir -p ../../bin
+mkdir ../../../fastlane
 echo "Install script wrappers:"
 cp -pvn ci-scripts_caller.sh.dist ../../bin/buildApk.sh
 cp -pvn ../templates/DeclarativePipeline.Jenkinsfile ../../../Jenkinsfile
 cp -pvn ../templates/sonar-project.properties ../../../sonar-project.properties
+cp -pvn ../templates/Gemfile ../../../Gemfile
+cp -pvn ../templates/Gemfile.lock ../../../Gemfile.lock
+for filename in Appfile Fastfile Pluginfile README.md
+do
+    cp -pvn ../templates/fastlane/${filename} ../../../fastlane/${filename}
+done
