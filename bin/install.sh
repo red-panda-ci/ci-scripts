37be8a30de1f8842708f46f591c908004ece27de
Pedro Amador <pedroamador.rodriguez@gmail.com>
2017-02-26 20:41:36 +0100

    Fix install cp options

--- bin/install.sh
+++ bin/install.sh
@@ -4,4 +4,4 @@ mkdir -p ../../bin
 echo "Install script wrappers:"
 cp -pvn ci-scripts_caller.sh.dist ../../bin/buildApk.sh
 cp -pvn ../templates/DeclarativePipeline.Jenkinsfile ../../../Jenkinsfile
-cp -pvh ../templates/sonar-project.properties ../../../sonar-project.properties
+cp -pvn ../templates/sonar-project.properties ../../../sonar-project.properties
